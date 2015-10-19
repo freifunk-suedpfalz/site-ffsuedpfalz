759897bb43532d4389bfb172330b26640161b1df
pinguinpfleger <kai@pinguinpfleger.de>
2015-10-19 16:02:42 +0200

    stable 0.1.0

--- site.mk
+++ site.mk
@@ -1,28 +1,28 @@
 GLUON_SITE_PACKAGES := \
-        gluon-mesh-batman-adv-15 \
-	gluon-alfred \
-	gluon-announced \
-	gluon-autoupdater \
-        gluon-config-mode-autoupdater \
-        gluon-config-mode-hostname \
-        gluon-config-mode-mesh-vpn \
-        gluon-config-mode-geo-location \
-        gluon-config-mode-contact-info \
-	gluon-ebtables-filter-multicast \
-	gluon-ebtables-filter-ra-dhcp \
-	gluon-luci-admin \
-	gluon-luci-autoupdater \
-	gluon-luci-portconfig \
-        gluon-luci-private-wifi \
-	gluon-next-node \
-	gluon-mesh-vpn-fastd \
-	gluon-radvd \
-	gluon-status-page \
-	gluon-setup-mode \
-	gluon-config-mode-core \
-	iwinfo \
-	iptables \
-	haveged
+  gluon-mesh-batman-adv-15 \
+  gluon-alfred \
+  gluon-announced \
+  gluon-autoupdater \
+  gluon-config-mode-autoupdater \
+  gluon-config-mode-hostname \
+  gluon-config-mode-mesh-vpn \
+  gluon-config-mode-geo-location \
+  gluon-config-mode-contact-info \
+  gluon-ebtables-filter-multicast \
+  gluon-ebtables-filter-ra-dhcp \
+  gluon-luci-admin \
+  gluon-luci-autoupdater \
+  gluon-luci-portconfig \
+  gluon-luci-private-wifi \
+  gluon-next-node \
+  gluon-mesh-vpn-fastd \
+  gluon-radvd \
+  gluon-status-page \
+  gluon-setup-mode \
+  gluon-config-mode-core \
+  iwinfo \
+  iptables \
+  haveged
 
 GLUON_BRANCH ?= stable
 DEFAULT_GLUON_RELEASE := 0.1.0~$(GLUON_BRANCH)_$(shell date '+%Y%m%d')
