b56e86031bc7caa34d792bbe7be86546d323858d
Bobo-PK <cia@mailbox.org>
2015-11-16 10:53:04 +0100

    Update site.mk
    DEFAULT_GLUON_RELEASE := 0.1.1

--- site.mk
+++ site.mk
@@ -25,7 +25,7 @@ GLUON_SITE_PACKAGES := \
   haveged
 
 GLUON_BRANCH ?= stable
-DEFAULT_GLUON_RELEASE := 0.1.0~$(GLUON_BRANCH)_$(shell date '+%Y%m%d')
+DEFAULT_GLUON_RELEASE := 0.1.1~$(GLUON_BRANCH)_$(shell date '+%Y%m%d')
 
 # Allow overriding the release number from the command line
 GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
