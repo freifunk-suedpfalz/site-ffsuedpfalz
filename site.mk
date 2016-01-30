a95e027a523270c54e84a3daed1e25dace6e8301
pinguinpfleger <freifunk@pinguinpfleger.de>
2016-01-30 19:23:40 +0100

    Anpassungen für gluon 2016.1 site.conf: Wifi Konfiguration angepasst site.mk gluon-luci-wifi-config hinzugefügt

--- site.mk
+++ site.mk
@@ -4,28 +4,29 @@ GLUON_SITE_PACKAGES := \
   gluon-announced \
   gluon-autoupdater \
   gluon-config-mode-autoupdater \
+  gluon-config-mode-contact-info \
+  gluon-config-mode-core \
+  gluon-config-mode-geo-location \
   gluon-config-mode-hostname \
   gluon-config-mode-mesh-vpn \
-  gluon-config-mode-geo-location \
-  gluon-config-mode-contact-info \
   gluon-ebtables-filter-multicast \
   gluon-ebtables-filter-ra-dhcp \
   gluon-luci-admin \
   gluon-luci-autoupdater \
   gluon-luci-portconfig \
+  gluon-luci-wifi-config \
   gluon-luci-private-wifi \
   gluon-next-node \
   gluon-mesh-vpn-fastd \
   gluon-radvd \
-  gluon-status-page \
   gluon-setup-mode \
-  gluon-config-mode-core \
-  iwinfo \
+  gluon-status-page \
+  haveged \
   iptables \
-  haveged
+  iwinfo
 
-GLUON_BRANCH ?= stable
-DEFAULT_GLUON_RELEASE := 0.1.6~$(GLUON_BRANCH)_$(shell date '+%Y%m%d')
+GLUON_BRANCH ?= experimental
+DEFAULT_GLUON_RELEASE := 0.1.7~$(GLUON_BRANCH)_$(shell date '+%Y%m%d')
 
 # Allow overriding the release number from the command line
 GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
