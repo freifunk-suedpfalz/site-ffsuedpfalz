655230fc1ab5506a58b890a1d10740bf3fda11f8
kai <kai@pinguinpfleger.de>
2016-09-27 17:49:59 +0200

    add addition network drivers and usb stuff for x86 builds

--- site.mk
+++ site.mk
@@ -25,6 +25,38 @@ GLUON_SITE_PACKAGES := \
   iptables \
   iwinfo
 
+
+# add addition network drivers and usb stuff only to targes where disk space does not matter.
+ifeq ($(GLUON_TARGET),x86-generic)
+GLUON_SITE_PACKAGES += \
+        kmod-usb-core \
+        kmod-usb2 \
+        kmod-usb-hid \
+        kmod-usb-net \
+        kmod-usb-net-asix \
+        kmod-usb-net-dm9601-ether \
+        kmod-sky2 \
+        kmod-r8169 \
+        kmod-forcedeth \
+        kmod-8139too \
+	kmod-atl2
+endif
+
+ifeq ($(GLUON_TARGET),x86-64)
+GLUON_SITE_PACKAGES += \
+        kmod-usb-core \
+        kmod-usb2 \
+        kmod-usb-hid \
+        kmod-usb-net \
+        kmod-usb-net-asix \
+        kmod-usb-net-dm9601-ether \
+        kmod-sky2 \
+        kmod-r8169 \
+        kmod-forcedeth \
+        kmod-8139too \
+	kmod-atl2
+endif
+
 GLUON_BRANCH ?= experimental
 FWVER ?= 0.2.2
 DEFAULT_GLUON_RELEASE := ${FWVER}~$(GLUON_BRANCH)_$(shell date '+%Y%m%d')
@@ -32,8 +64,10 @@ DEFAULT_GLUON_RELEASE := ${FWVER}~$(GLUON_BRANCH)_$(shell date '+%Y%m%d')
 # Allow overriding the release number from the command line
 GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
 
+GLUON_PRIORITY ?= 0
+GLUON_LANGS ?= de en
+
 # Region code required for some images; supported values: us eu
 GLUON_REGION ?= eu
 
-GLUON_PRIORITY ?= 0
-GLUON_LANGS ?= de en
+GLUON_ATH10K_MESH ?= 11s
