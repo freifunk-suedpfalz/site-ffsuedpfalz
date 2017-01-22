1eb0f0ea7c69472ce56a8741b1802c5c5b14b005
holymoly <holzmolz@googlemail.com>
2017-01-22 21:13:48 +0100

    switch to l2tp

--- site.mk
+++ site.mk
@@ -2,37 +2,85 @@ GLUON_SITE_PACKAGES := \
   gluon-mesh-batman-adv-15 \
   gluon-alfred \
   gluon-respondd \
+  ffho-ath9k-blackout-workaround \
+  gluon-autorestart \
   gluon-autoupdater \
   gluon-config-mode-autoupdater \
   gluon-config-mode-contact-info \
   gluon-config-mode-core \
   gluon-config-mode-geo-location \
   gluon-config-mode-hostname \
-  gluon-config-mode-mesh-vpn \
+  gluon-config-mode-tunneldigger \
   gluon-ebtables-filter-multicast \
   gluon-ebtables-filter-ra-dhcp \
+  gluon-ebtables-filter-roguenets \
   gluon-luci-admin \
   gluon-luci-autoupdater \
   gluon-luci-portconfig \
   gluon-luci-wifi-config \
   gluon-luci-private-wifi \
   gluon-next-node \
-  gluon-mesh-vpn-fastd \
+  gluon-mesh-vpn-tunneldigger \
+  gluon-tunneldigger-watchdog \
   gluon-radvd \
   gluon-setup-mode \
   gluon-status-page \
-  haveged \
+  gluon-migrate-vpn \
+  iwinfo \
   iptables \
-  iwinfo
+  haveged
 
-GLUON_BRANCH ?= experimental
-DEFAULT_GLUON_RELEASE := 0.1.8~$(GLUON_BRANCH)_$(shell date '+%Y%m%d')
+USB_BASIC := \
+  kmod-usb-core \
+  kmod-usb2 \
+  kmod-usb-hid
+
+USB_NIC := \
+  kmod-usb-net \
+  kmod-usb-net-asix \
+  kmod-usb-net-rtl8150 \
+  kmod-usb-net-rtl8152 \
+  kmod-usb-net-dm9601-ether
+
+USB_WIFI := \
+  kmod-rtl8192cu
+
+ifeq ($(GLUON_TARGET),x86-generic)
+  GLUON_SITE_PACKAGES += \
+    $(USB_BASIC) \
+    kmod-usb-ohci-pci \
+    $(USB_NIC)
+endif
+
+ifeq ($(GLUON_TARGET),x86-64)
+  GLUON_SITE_PACKAGES += \
+    $(USB_BASIC) \
+    $(USB_NIC) \
+    kmod-igb #APU2
+endif
+
+ifeq ($(GLUON_TARGET),brcm2708-bcm2708)
+  GLUON_SITE_PACKAGES += \
+    $(USB_BASIC) \
+    $(USB_NIC) \
+    $(USB_WIFI)
+endif
+
+ifeq ($(GLUON_TARGET),brcm2708-bcm2709)
+  GLUON_SITE_PACKAGES += \
+    $(USB_BASIC) \
+    $(USB_NIC) \
+    $(USB_WIFI)
+endif
+
+DEFAULT_GLUON_RELEASE := v1.2.X-exp-$(shell date '+%Y%m%d')
 
 # Allow overriding the release number from the command line
 GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
 
-# Region code required for some images; supported values: us eu
-GLUON_REGION ?= eu
-
+# Default priority for updates.
 GLUON_PRIORITY ?= 0
-GLUON_LANGS ?= de en
+
+GLUON_LANGS ?= en de
+
+GLUON_ATH10K_MESH ?= ibss
