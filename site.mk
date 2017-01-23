c30c378ba1ffc8028c4843813ff0b9f7f5a8de3f
holymoly <holzmolz@googlemail.com>
2017-01-23 20:27:39 +0100

    update dependencies for 2016.2.x

--- site.mk
+++ site.mk
@@ -3,7 +3,6 @@ GLUON_SITE_PACKAGES := \
   gluon-alfred \
   gluon-respondd \
   ffho-ath9k-blackout-workaround \
-  gluon-autorestart \
   gluon-autoupdater \
   gluon-config-mode-autoupdater \
   gluon-config-mode-contact-info \
@@ -42,9 +41,6 @@ USB_NIC := \
   kmod-usb-net-rtl8152 \
   kmod-usb-net-dm9601-ether
 
-USB_WIFI := \
-  kmod-rtl8192cu
-
 ifeq ($(GLUON_TARGET),x86-generic)
   GLUON_SITE_PACKAGES += \
     $(USB_BASIC) \
@@ -59,20 +55,6 @@ ifeq ($(GLUON_TARGET),x86-64)
     kmod-igb #APU2
 endif
 
-ifeq ($(GLUON_TARGET),brcm2708-bcm2708)
-  GLUON_SITE_PACKAGES += \
-    $(USB_BASIC) \
-    $(USB_NIC) \
-    $(USB_WIFI)
-endif
-
-ifeq ($(GLUON_TARGET),brcm2708-bcm2709)
-  GLUON_SITE_PACKAGES += \
-    $(USB_BASIC) \
-    $(USB_NIC) \
-    $(USB_WIFI)
-endif
-
 DEFAULT_GLUON_RELEASE := v1.2.X-exp-$(shell date '+%Y%m%d')
 
 # Allow overriding the release number from the command line
@@ -82,5 +64,3 @@ GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
 GLUON_PRIORITY ?= 0
 
 GLUON_LANGS ?= en de
-
-GLUON_ATH10K_MESH ?= ibss
