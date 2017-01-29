3ceb2f44ed2cad53240d4ec4c638bdde5fb34889
holymoly <holzmolz@googlemail.com>
2017-01-29 19:24:12 +0100

    Update build-jenkins.sh

--- build-jenkins.sh
+++ build-jenkins.sh
@@ -40,7 +40,7 @@ cp "$WORKSPACE/site.conf" "$WORKSPACE/gluon/site/"
 # Gluon Pakete aktualisieren und Build ausfuhren 
 cd "$WORKSPACE/gluon"
 make update "GLUON_RELEASE=$GLUON_RELEASE"  
-make clean GLUON_TARGET=ar71xx-generic "GLUON_RELEASE=$GLUON_RELEASE" 
+make clean V=s GLUON_TARGET=ar71xx-generic
 make -j5 V=s GLUON_TARGET=ar71xx-generic GLUON_BRANCH=experimental "GLUON_RELEASE=$GLUON_RELEASE"
 # make V=s "GLUON_RELEASE=$GLUON_RELEASE" "GLUON_BRANCH=stable"
 
