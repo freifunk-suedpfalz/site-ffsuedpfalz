2c1c1c1d4c10e44aecc4e721b8859065577f700c
Pinguinpfleger <kai@pinguinpfleger.de>
2017-08-30 23:32:46 +0200

    lede init

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 Gluon site config für Freifunk Haßloch in der Queichufer Domain des Freifunk Rheinland e.V.
 
-Der v2016.2 branch ist für gluon v2016.2 builds
+Der v2017.1 branch ist für gluon v2017.1 builds
--- site.mk
+++ site.mk
@@ -1,59 +1,68 @@
+GLUON_FEATURES := \
+	autoupdater \
+	ebtables-filter-multicast \
+	ebtables-filter-ra-dhcp \
+	mesh-batman-adv-15 \
+	mesh-vpn-fastd \
+	radvd \
+	respondd \
+	status-page \
+	web-advanced \
+	web-wizard
+
 GLUON_SITE_PACKAGES := \
-  gluon-mesh-batman-adv-15 \
-  gluon-alfred \
-  gluon-respondd \
-  gluon-autoupdater \
-  gluon-config-mode-autoupdater \
-  gluon-config-mode-contact-info \
-  gluon-config-mode-core \
-  gluon-config-mode-geo-location \
-  gluon-config-mode-hostname \
-  gluon-config-mode-mesh-vpn \
-  gluon-ebtables-filter-multicast \
-  gluon-ebtables-filter-ra-dhcp \
-  gluon-luci-admin \
-  gluon-luci-autoupdater \
-  gluon-luci-portconfig \
-  gluon-luci-wifi-config \
-  gluon-luci-private-wifi \
-  gluon-next-node \
-  gluon-mesh-vpn-fastd \
-  gluon-radvd \
-  gluon-setup-mode \
-  gluon-status-page \
-  haveged \
-  iptables \
-  iwinfo
+	gluon-alfred \
+	gluon-respondd \
+	gluon-autoupdater \
+	gluon-config-mode-autoupdater \
+	gluon-config-mode-contact-info \
+	gluon-config-mode-core \
+	gluon-config-mode-geo-location \
+	gluon-config-mode-hostname \
+	gluon-config-mode-mesh-vpn \
+	gluon-ebtables-filter-multicast \
+	gluon-ebtables-filter-ra-dhcp \
+	gluon-web-admin \
+	gluon-web-autoupdater \
+	gluon-web-network \
+	gluon-web-wifi-config \
+	gluon-mesh-batman-adv-15 \
+	gluon-mesh-vpn-fastd \
+	gluon-radvd \
+	gluon-setup-mode \
+	gluon-status-page \
+	haveged \
+	iwinfo
 
 
 # add addition network drivers and usb stuff only to targes where disk space does not matter.
 ifeq ($(GLUON_TARGET),x86-generic)
 GLUON_SITE_PACKAGES += \
-        kmod-usb-core \
-        kmod-usb2 \
-        kmod-usb-hid \
-        kmod-usb-net \
-        kmod-usb-net-asix \
-        kmod-usb-net-dm9601-ether \
-        kmod-sky2 \
-        kmod-r8169 \
-        kmod-forcedeth \
-        kmod-8139too \
+	kmod-usb-core \
+	kmod-usb2 \
+	kmod-usb-hid \
+	kmod-usb-net \
+	kmod-usb-net-asix \
+	kmod-usb-net-dm9601-ether \
+	kmod-sky2 \
+	kmod-r8169 \
+	kmod-forcedeth \
+	kmod-8139too \
 	kmod-atl2
 endif
 
 ifeq ($(GLUON_TARGET),x86-64)
 GLUON_SITE_PACKAGES += \
-        kmod-usb-core \
-        kmod-usb2 \
-        kmod-usb-hid \
-        kmod-usb-net \
-        kmod-usb-net-asix \
-        kmod-usb-net-dm9601-ether \
-        kmod-sky2 \
-        kmod-r8169 \
-        kmod-forcedeth \
-        kmod-8139too \
+	kmod-usb-core \
+	kmod-usb2 \
+	kmod-usb-hid \
+	kmod-usb-net \
+	kmod-usb-net-asix \
+	kmod-usb-net-dm9601-ether \
+	kmod-sky2 \
+	kmod-r8169 \
+	kmod-forcedeth \
+	kmod-8139too \
 	kmod-atl2
 endif
 
