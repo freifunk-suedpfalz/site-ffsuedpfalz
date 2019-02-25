e261851ae2b7556ff7d71e51b18ff8bbd4c70666
holymoly <holzmolz@googlemail.com>
2019-02-25 10:17:34 +0100

    intermediate fw for cpe router

--- site.mk
+++ site.mk
@@ -1,35 +1,86 @@
-GLUON_FEATURES := \
-  autoupdater \
-  alfred \
-  config-mode-domain-select \
-  ebtables-filter-multicast \
-  ebtables-filter-ra-dhcp \
-  radv-filterd\
-  ebtables-limit-arp \
-  mesh-batman-adv-15 \
-  mesh-vpn-tunneldigger \
-  radvd \
-  respondd \
-  status-page \
-  web-advanced \
-  web-wizard \
-  web-osm \
-  config-mode-geo-location-osm
 
 GLUON_SITE_PACKAGES := \
+  gluon-mesh-batman-adv-15 \
+  gluon-alfred \
+  gluon-respondd \
   gluon-autorestart \
+  gluon-autoupdater \
+  gluon-config-mode-autoupdater \
+  gluon-config-mode-contact-info \
+  gluon-config-mode-core \
+  gluon-config-mode-geo-location \
+  gluon-config-mode-hostname \
+  gluon-config-mode-mesh-vpn \
+  gluon-ebtables-filter-multicast \
+  gluon-ebtables-filter-ra-dhcp \
+  gluon-ebtables-source-filter \
+  gluon-web-admin \
+  gluon-web-autoupdater \
+  gluon-web-wifi-config \
+  gluon-web-private-wifi \
+  gluon-web-network \
+  gluon-mesh-vpn-tunneldigger \
   gluon-tunneldigger-watchdog \
+  gluon-radvd \
+  gluon-setup-mode \
+  gluon-status-page \
+  respondd-module-airtime \
   iwinfo \
   haveged
+  
+USB_BASIC := \
+  kmod-usb-core \
+  kmod-usb2 \
+  kmod-usb-hid
 
-GLUON_BRANCH ?= experimental
-DEFAULT_GLUON_RELEASE := 0.1.8~$(GLUON_BRANCH)_$(shell date '+%Y%m%d')
+USB_NIC := \
+  kmod-usb-net \
+  kmod-usb-net-asix \
+  kmod-usb-net-rtl8150 \
+  kmod-usb-net-rtl8152 \
+  kmod-usb-net-dm9601-ether
+
+USB_WIFI := \
+  kmod-rtl8192cu
+
+ifeq ($(GLUON_TARGET),x86-generic)
+  GLUON_SITE_PACKAGES += \
+    $(USB_BASIC) \
+    kmod-usb-ohci-pci \
+    $(USB_NIC)
+endif
+
+ifeq ($(GLUON_TARGET),x86-64)
+  GLUON_SITE_PACKAGES += \
+    $(USB_BASIC) \
+    $(USB_NIC) \
+    kmod-igb #APU2
+endif
+
+ifeq ($(GLUON_TARGET),brcm2708-bcm2708)
+  GLUON_SITE_PACKAGES += \
+    $(USB_BASIC) \
+    $(USB_NIC) \
+    $(USB_WIFI)
+endif
+
+ifeq ($(GLUON_TARGET),brcm2708-bcm2709)
+  GLUON_SITE_PACKAGES += \
+    $(USB_BASIC) \
+    $(USB_NIC) \
+    $(USB_WIFI)
+endif
+
+DEFAULT_GLUON_RELEASE := v0.1.X-exp-$(shell date '+%Y%m%d')
 
 # Allow overriding the release number from the command line
 GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
 
-# Region code required for some images; supported values: us eu
-GLUON_REGION ?= eu
-
+# Default priority for updates.
 GLUON_PRIORITY ?= 0
-GLUON_LANGS ?= de en
+
+GLUON_LANGS ?= en de
+
+GLUON_ATH10K_MESH ?= 11s
+
+GLUON_REGION := eu
