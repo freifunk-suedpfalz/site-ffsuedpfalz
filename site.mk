ec540fe515a1171ccbe3a7f1ebb2dd960ae2c7f6
holymoly <holzmolz@googlemail.com>
2019-02-25 10:12:48 +0100

    intermediate fw for cpe router

--- site.mk
+++ site.mk
@@ -1,29 +1,26 @@
+GLUON_FEATURES := \
+  autoupdater \
+  alfred \
+  config-mode-domain-select \
+  ebtables-filter-multicast \
+  ebtables-filter-ra-dhcp \
+  radv-filterd\
+  ebtables-limit-arp \
+  mesh-batman-adv-15 \
+  mesh-vpn-tunneldigger \
+  radvd \
+  respondd \
+  status-page \
+  web-advanced \
+  web-wizard \
+  web-osm \
+  config-mode-geo-location-osm
+
 GLUON_SITE_PACKAGES := \
-  gluon-mesh-batman-adv-15 \
-  gluon-alfred \
-  gluon-respondd \
-  gluon-autoupdater \
-  gluon-config-mode-autoupdater \
-  gluon-config-mode-contact-info \
-  gluon-config-mode-core \
-  gluon-config-mode-geo-location \
-  gluon-config-mode-hostname \
-  gluon-config-mode-mesh-vpn \
-  gluon-ebtables-filter-multicast \
-  gluon-ebtables-filter-ra-dhcp \
-  gluon-luci-admin \
-  gluon-luci-autoupdater \
-  gluon-luci-portconfig \
-  gluon-luci-wifi-config \
-  gluon-luci-private-wifi \
-  gluon-next-node \
-  gluon-mesh-vpn-fastd \
-  gluon-radvd \
-  gluon-setup-mode \
-  gluon-status-page \
-  haveged \
-  iptables \
-  iwinfo
+  gluon-autorestart \
+  gluon-tunneldigger-watchdog \
+  iwinfo \
+  haveged
 
 GLUON_BRANCH ?= experimental
 DEFAULT_GLUON_RELEASE := 0.1.8~$(GLUON_BRANCH)_$(shell date '+%Y%m%d')
