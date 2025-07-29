228ac8132f9067a2545754115f22ff6d5d561704
holymoly <holzmolz@googlemail.com>
2025-07-29 21:20:44 +0200

    Update site.mk

--- site.mk
+++ site.mk
@@ -1,20 +1,3 @@
-USB_BASIC := \
-	kmod-usb-core \
-	kmod-usb2 \
-	:xkmod-usb-hid
-
-USB_NIC := \
-  kmod-usb-net \
-  kmod-usb-net-asix \
-  kmod-usb-net-rtl8150 \
-  kmod-usb-net-rtl8152 \
-  kmod-usb-net-dm9601-ether
-
-USB_WIFI := \
-  kmod-rtl8192cu
-
-DEFAULT_GLUON_RELEASE := 1.4.0~$(GLUON_BRANCH)_$(shell date '+%Y%m%d')
-
 # Default priority for updates.
 GLUON_PRIORITY ?= 0
 
